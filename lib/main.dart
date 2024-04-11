0fc0808acffe5ab2d88c4305e4a2cf5fe6fff87d
monikabj <monikavh.ti22@utsjr.edu.mx>
2024-04-11 12:32:59 -0600

    Cambios16

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:live_reciclying/pages/splash.dart';
+import 'package:live_reciclying/widgets/social_login.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 import 'mongodb.dart';
 
