45e00819b224ff8cf5c27f332b67e85a9ec33d31
monikabj <monikavh.ti22@utsjr.edu.mx>
2024-04-12 12:16:38 -0600

    Cambios19

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,3 @@
-const MONGO_URL="mongodb+srv://admin:<password>@lrectr002.wfvwsjw.mongodb.net/?retryWrites=true&w=majority&appName=LRectr002";
+const MONGO_URL="mongodb+srv://admin:admin123@lrectr002.wfvwsjw.mongodb.net/?retryWrites=true&w=majority&appName=LRectr002";
 const COLLECTION_NAME="usuarios";
 
